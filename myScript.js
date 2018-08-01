e14462e1321df27a197514d8058885f3cc574df1
GWC22 <gwc22@GWC22s-MacBook-Air.local>
2018-08-01 10:40:00 -0400

    Initial commit

---
+++ myScript.js
@@ -0,0 +1,8 @@
+function readyplayeronefunction(){
+  document.getElementById("buyreadyplayerone").innerHTML= <a href="https://www.amazon.com/Ready-Player-One-Ernest-Cline/dp/0307887448" target="blank"><em>Amazon</em></a>;
+}
+
+
+function warcrossfunction() {
+  document.getElementById("buywarcross").innerHTML= <a href="https://www.amazon.com/Warcross-Marie-Lu/dp/0399547967" target="blank"><em>Amazon</em></a> ;
+}
